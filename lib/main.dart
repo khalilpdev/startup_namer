79e291e592411f02dc7a69150e961f2e2d10e169
Leandro Khalil <khalilpdev@gmail.com>
2022-04-19 23:16:01 -0300

    add icons to the list

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -47,24 +48,11 @@ class _RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16),
-      // The itemBuilder callback is called once per suggested
-      // word pairing, and places each suggestion into a ListTile
-      // row. For even rows, the function adds a ListTile row for
-      // the word pairing. For odd rows, the function adds a
-      // Divider widget to visually separate the entries. Note that
-      // the divider may be difficult to see on smaller devices.
       itemBuilder: (context, i) {
-        // Add a one-pixel-high divider widget before each row
-        // in the ListView.
         if (i.isOdd) {
           return const Divider();
         }
 
-        // The syntax "i ~/ 2" divides i by 2 and returns an
-        // integer result.
-        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-        // This calculates the actual number of word pairings
-        // in the ListView,minus the divider widgets.
         final index = i ~/ 2;
 
         if (index >= _suggestions.length) {
@@ -77,10 +65,16 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
       ),
     );
   }
